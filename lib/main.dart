ff137be0c5dba1583d3857105843dfba9fa32e4f
CtrlZeeT <18521494@gm.uit.edu.vn>
2021-05-27 18:31:55 +0700

    Main Screen quarter done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:health_care/MainScreen/main_screen.dart';
+import 'package:health_care/constants.dart';
 import 'package:health_care/sign_in/sign_in_screen.dart';
 
 void main() {
@@ -6,24 +8,17 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Health Care',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.indigo,
+        scaffoldBackgroundColor: bgColor,
       ),
-      home: SignInScreen(),
+
+      home: MainScreen(),
     );
   }
 }
