71f6bcb83e8118830cef70c9eb19dade69c40b7b
Cường Trương Bá <18520013@ms.uit.edu.vn>
2021-06-16 02:06:17 +0700

    Init Users to firestore

--- lib/landing_screen.dart
+++ lib/landing_screen.dart
@@ -1,13 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:health_care/MainScreen/main_screen.dart';
 import 'package:health_care/home_screen.dart';
 import 'package:health_care/service/auth.dart';
 import 'package:health_care/sign_in/sign_in_screen.dart';
+var ref = FirebaseFirestore.instance.collection('users');
 
 class LandingScreen extends StatelessWidget {
-  const LandingScreen({Key key, @required this.auth}) : super(key: key);
+  LandingScreen({Key key, @required this.auth}) : super(key: key);
   final AuthBase auth;
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User>(
@@ -15,14 +18,15 @@ class LandingScreen extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.active) {
           final User user = snapshot.data;
-          if (user == null)
-            return SignInScreen(
+          if (user != null){
+            _createUserIfNotExist(user);
+            return MainScreen(
               auth: auth,
-
             );
-          return MainScreen(
-            auth: auth,
+          }
 
+          return SignInScreen(
+            auth: auth,
           );
         }
         return Scaffold(
@@ -31,5 +35,18 @@ class LandingScreen extends StatelessWidget {
       },
     );
 
+  }
+  Future<void> _createUserIfNotExist(User user) async{
+    ref.doc(user.uid).get().then((DocumentSnapshot documentSnapshot) {
+      if (!documentSnapshot.exists){
+        ref.add({
+          'id': user.uid,
+          'full name': "Chưa cập nhật",
+          'phone number': "",
+          'image profile': "",
+          'sex' : "Chưa cập nhật",
+        });
+      }
+    });
   }
 }
