c5ba8ac0e2afc8110278952405369d314b383ee5
CtrlZeeT <18521494@gm.uit.edu.vn>
2021-06-16 19:20:35 +0700

    Hotfix for main screen

--- lib/MainScreen/main_screen.dart
+++ lib/MainScreen/main_screen.dart
@@ -1,9 +1,8 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:health_care/constants.dart';
 import 'package:health_care/service/auth.dart';
 import 'package:health_care/sign_in/sign_in_screen.dart';
+import 'DataSearch.dart';
 
 class HeathFunctionClassInfo {
   final String Name, ImageURL;
@@ -55,17 +54,91 @@ class MainScreen extends StatelessWidget {
         ),
         actions: <Widget>[
           IconButton(
-              icon: Icon(
-                Icons.logout,
-                color: Colors.white,
-              ),
-              onPressed: _signOut,
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+            onPressed: _signOut,
           ),
         ],
       ),
       body: ListView(
         children: <Widget>[
           SizedBox(height: defaultPadding),
+          Container(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Text("Tìm kiếm thông tin về sức khỏe",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ))),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+            child: OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.indigo,
+                minimumSize:
+                Size(MediaQuery.of(context).size.width * 0.8, 26.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: DataSearch(
+                    hintText: "Tìm kiếm",
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 8.0,
+                  bottom: 8.0,
+                ),
+                child: Transform(
+                  transform: Matrix4.translationValues(-8, 0.0, 0.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.search_sharp,
+                        color: Colors.white,
+                        size: 36,
+                      ),
+                      SizedBox(
+                        width: 4.0,
+                      ),
+                      Text(
+                        "Tìm kiếm",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: defaultPadding),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text(
+              "Theo dõi thông tin sức khỏe",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 18),
+            ),
+          ),
+          SizedBox(height: 8.0),
           Container(
             color: panelColor,
             height: 160.0,
